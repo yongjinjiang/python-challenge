e91ee9493d5369b1fb826c25e5f9cb6a24a6d966
Yongjin <yjjiangphysics@gmail>
2018-09-04 00:09:49 -0500

    Second assignment, PyPoll, is truly done

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -12,13 +12,21 @@ with open(file_path,newline="") as data_file:
     Votes=[] 
     for row in csvreader:
          Votes.append(row[2])
+    
+    f=open('election_result.txt','w')
+
     print("Election Results")
+    f.write("Election Results\n")
     print("____________________")
+    f.write("____________________\n")
     Tot=len(Votes)
     print("Total Votes:{}".format(Tot))
+    f.write("Total Votes:{}\n".format(Tot))
     print("____________________")
+    f.write("____________________\n")
     candidates = set(Votes)
     print(f"candidates: {candidates}")
+    f.write(f"candidates: {candidates}\n")
     
     winner=list(candidates)[0]
     ss0=Votes.count(winner)
@@ -26,12 +34,16 @@ with open(file_path,newline="") as data_file:
         ss=Votes.count(name)
         ss1=ss/Tot
         print(f"{name}: "+ "{0:.0%}".format(ss1)+f" ({ss})")
+        f.write(f"{name}: "+ "{0:.0%}".format(ss1)+f" ({ss})\n")
         if ss>ss0:
             winner=name
             ss0=ss
-
     print("____________________")
+    f.write("____________________\n")
     print(f"Winner:  {winner}")
+    f.write(f"Winner:  {winner}\n")
     print("____________________")
-
+    f.write("____________________\n")
+    f.close()
+    
 
